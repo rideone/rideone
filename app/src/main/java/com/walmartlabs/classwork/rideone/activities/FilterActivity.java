d176a6547356d94d024b1dadd13cdce8d34a1d35
dmaskevich <dmaskevich@walmartlabs.com>
2015-11-30 02:30:05 -0800

    style changes

--- app/src/main/java/com/walmartlabs/classwork/rideone/activities/FilterActivity.java
+++ app/src/main/java/com/walmartlabs/classwork/rideone/activities/FilterActivity.java
@@ -2,7 +2,9 @@ package com.walmartlabs.classwork.rideone.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,15 +13,26 @@ import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.walmartlabs.classwork.rideone.R;
 import com.walmartlabs.classwork.rideone.models.Filter;
+import com.walmartlabs.classwork.rideone.models.User;
 
 public class FilterActivity extends AppCompatActivity {
+
+    User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        user = ((User)getIntent().getSerializableExtra("user")).rebuild();
         setupSpinners();
     }
 
@@ -30,6 +43,8 @@ public class FilterActivity extends AppCompatActivity {
         Spinner spTimeRange = createSpinnerFromResource(R.id.spTimeRange, R.array.timerange, Filter.getTimeRange());
         Switch swFilterOnOff = (Switch) findViewById(R.id.swFilterOnOff);
         swFilterOnOff.setChecked(Filter.isFilterOn());
+        TextView tvSpots = (TextView) findViewById(R.id.tvSpots);
+        swFilterOnOff.setTextColor(tvSpots.getTextColors());
 
         swFilterOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -109,11 +124,23 @@ public class FilterActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Intent upIntent = NavUtils.getParentActivityIntent(this);
+            upIntent.putExtra("user", user.flush());
+            NavUtils.navigateUpTo(this, upIntent);
+            return true;
+        } else if (id == R.id.mi_save){
+            onSave();
+        }
+
+
+    return super.onOptionsItemSelected(item);
     }
 
-    public void onSave(View view) {
+    public void onSave() {
         Intent i = new Intent();
+        i.putExtra("user", user.flush());
         setResult(RESULT_OK, i);
         finish();
     }
