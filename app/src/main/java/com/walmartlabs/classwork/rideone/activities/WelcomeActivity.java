e42ea36c0d092aaef2f90a3df5e787390aff1b98
dmaskevich <dmaskevich@walmartlabs.com>
2015-11-30 18:38:20 -0800

    add welcome screen

---
+++ app/src/main/java/com/walmartlabs/classwork/rideone/activities/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.walmartlabs.classwork.rideone.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.walmartlabs.classwork.rideone.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
